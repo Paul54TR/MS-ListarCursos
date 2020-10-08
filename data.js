226a03257615567861bbd418c44de8c9aa445bde
Paul Tijero <paultijero@gmail.com>
2020-10-08 11:34:23 -0500

    Testing

---
+++ data.js
@@ -0,0 +1,8 @@
+var datos = [
+    {id: 1 , name: "Matematica"},
+    {id: 2 , name: "Comunicación"},
+    {id: 3 , name: "Ciencia, Tecnologia y Ambiente"},
+    {id: 4 , name: "Educación Fisica"},
+    {id: 5 , name: "Historia"}
+]
+module.export = datos;
